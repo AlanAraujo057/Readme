f289a35544264349b0ae7fdace0c4ea4949bc45f
AlanAraujo057 <20243ds0576@utez.edu.mx>
2025-05-21 09:03:04 -0600

    promedio de 3 numeros

---
+++ promedio/.idea/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Double num1= Double.valueOf(JOptionPane.showInputDialog("Dame un numero"));
+    Double num2= Double.valueOf(JOptionPane.showInputDialog("Dame el segundo numero"));
+    Double num3= Double.valueOf(JOptionPane.showInputDialog("Dame el tercer numero"));
+
+    System.out.println((Double.valueOf(num1)+Double.valueOf(num2)+Double.valueOf(num3))/3);
+ // Alan Araujo Ocampo
+    }
+}
